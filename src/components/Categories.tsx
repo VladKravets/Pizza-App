3e1bf536d03d410f7af8880e0cd4844682c72597
Vlad Kravets <hazardvlad19@gmail.com>
2022-05-24 15:52:54 +0300

    change render categories

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Categories = () => {
+    const [activeIndex, setActiveIndex] = useState(3)
+
+    const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые',]
+    const onClickCategories = (index: number) => {
+        setActiveIndex(index)
+    }
     return (
         <div className="categories">
             <ul>
-                <li className="">Все</li>
-                <li className="">Мясные</li>
-                <li className="active">Вегетарианская</li>
-                <li className="">Гриль</li>
-                <li className="">Острые</li>
-                <li className="">Закрытые</li>
+                {categories.map((cat,index) => {
+                    return <li
+                        key={index}
+                        onClick={() => onClickCategories(index)}
+                        className={activeIndex === index ? 'active' : ''}>{cat}
+                    </li>
+                })}
+
             </ul>
         </div>
     );
