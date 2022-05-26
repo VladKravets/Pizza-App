757f9a9cc7f9184d951ea199c64fd76ee053c675
Vlad Kravets <hazardvlad19@gmail.com>
2022-05-26 17:13:52 +0300

    any changes

---
+++ src/pages/Search-pizza/Search-pizza.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import style from "./SearchPizza.module.scss"
+
+const SearchPizza = () => {
+    return (
+        <div className={style.root}>
+            <svg
+                className={style.icon}
+                viewBox="0 0 32 32"
+                xmlns="http://www.w3.org/2000/svg"><title/>
+                <g id="search">
+                    <path
+                        d="M29.71,28.29l-6.5-6.5-.07,0a12,12,0,1,0-1.39,1.39s0,.05,0,.07l6.5,6.5a1,1,0,0,0,1.42,0A1,1,0,0,0,29.71,28.29ZM14,24A10,10,0,1,1,24,14,10,10,0,0,1,14,24Z"/>
+                </g>
+            </svg>
+            <input
+                className={style.input}
+                type="text"
+                placeholder={'Поиск пиццы...'}/>
+        </div>
+    );
+};
+
+export default SearchPizza;
