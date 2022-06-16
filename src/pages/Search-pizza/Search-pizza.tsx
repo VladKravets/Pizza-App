27c682377e89a1ba55c67563806816ffdaeb0bbe
Vlad Kravets <hazardvlad19@gmail.com>
2022-06-16 17:10:32 +0300

    added lodash-debounce

--- src/pages/Search-pizza/Search-pizza.tsx
+++ src/pages/Search-pizza/Search-pizza.tsx
@@ -1,15 +1,23 @@
 import React, {ChangeEvent, useContext} from 'react';
-import style from "./SearchPizza.module.scss"
+import style from './SearchPizza.module.scss';
 import {SearchContext} from "../../App";
-
+import debounce from 'lodash.debounce';
 
 const SearchPizza = () => {
 
     const {searchValue, setSearchValue} = useContext<any>(SearchContext)
-
-
+    const inputRef = React.useRef<HTMLInputElement>(null);
+    const testDebounce = React.useCallback(
+        debounce(() => {
+            console.log('Hello')
+        }, 1000), []
+    )
     const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => setSearchValue(e.currentTarget.value)
-    const onClickClearInput = () => setSearchValue('')
+    const onClickClearInput = () => {
+        setSearchValue('')
+// @ts-ignore
+        inputRef.current.focus()
+    }
 
     return (
         <div className={style.root}>
@@ -23,21 +31,23 @@ const SearchPizza = () => {
                 </g>
             </svg>
             <input
+                ref={inputRef}
                 className={style.input}
                 type="text"
                 placeholder={'Поиск пиццы 🍕...'}
                 value={searchValue}
                 onChange={onChangeHandler}
-            />{searchValue &&
-            <svg
-                onClick={onClickClearInput}
-                className={style.clearIcon} height="48" viewBox="0 0 48 48" width="48"
-                xmlns="http://www.w3.org/2000/svg">
-                <path
-                    d="M38 12.83l-2.83-2.83-11.17 11.17-11.17-11.17-2.83 2.83 11.17 11.17-11.17 11.17 2.83 2.83 11.17-11.17 11.17 11.17 2.83-2.83-11.17-11.17z"/>
-                <path d="M0 0h48v48h-48z" fill="none"/>
-            </svg>
-        }
+            />
+            {searchValue &&
+                <svg
+                    onClick={onClickClearInput}
+                    className={style.clearIcon} height="48" viewBox="0 0 48 48" width="48"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M38 12.83l-2.83-2.83-11.17 11.17-11.17-11.17-2.83 2.83 11.17 11.17-11.17 11.17 2.83 2.83 11.17-11.17 11.17 11.17 2.83-2.83-11.17-11.17z"/>
+                    <path d="M0 0h48v48h-48z" fill="none"/>
+                </svg>
+            }
         </div>
     );
 };
